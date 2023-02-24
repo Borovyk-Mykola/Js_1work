3f1611c225a1d5d48a254de523b7878ea0c85923
Borovyk <borovik191188@gmail.com>
2023-02-24 21:50:50 +0200

    task10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body")
+const buttonChangeColor = document.querySelector(".change-color")
+const spanColor = document.querySelector(".color")
+
+
+buttonChangeColor.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  return spanColor.textContent = `${bodyEl.style.backgroundColor}`;
+})
